577fb04613750d7961d1efee9e1cfb8c3c1dd915
markorovi <markorovi24@gmail.com>
2021-04-19 17:10:22 -0700

    Test 5

--- Ide/cmake-build-debug/cmake_install.cmake
+++ Ide/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/marco/Marco/Primer-Proyecto-Datos-II/Ide
+# Install script for directory: /home/joel/Qt_proyects/Ide
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/marco/Marco/Primer-Proyecto-Datos-II/Ide/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/joel/Qt_proyects/Ide/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
